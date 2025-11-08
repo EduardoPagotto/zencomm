72e49a69a6fc1f64267b0bddd4496c93d98f1920
Edu Pagotto <edupagotto@gmail.com>
2025-11-08 00:46:06 -0300

    cliente sincrono ok

---
+++ client.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+'''
+Created on 20170119
+Update on 20251108
+@author: Eduardo Pagotto
+'''
+
+import logging
+import time
+from urllib.parse import urlparse
+
+from zen.header import ProtocolCode
+from zen.syncronos.protocol import Protocol
+from zen.syncronos.socket import socket_client
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s %(name)-12s %(levelname)-8s %(threadName)-16s %(funcName)-20s %(message)s',
+    datefmt='%H:%M:%S',
+)
+
+def main():
+
+    log = logging.getLogger('Client')
+
+    try:
+
+        url_parser = urlparse('unix:///tmp/teste0.sock')
+
+        sock = socket_client(url_parser, 10)
+
+        protocol = Protocol(sock)
+        #log.info(protocol.handShake())
+
+        protocol.sendString(ProtocolCode.COMMAND, 'ola 123')
+
+        idval, msg = protocol.receiveString()
+        log.info('Recebido id:%s msg:%s', str(idval), msg)
+
+        time.sleep(30)
+
+        # protocol.sendString(ProtocolCode.ERRO, 'Erro Critico')
+        # idVal, msg = protocol.receiveString()
+        # log.info('Recebido id:%s msg:%s', idVal, msg)
+
+        protocol.sendClose('Bye-Bye')
+
+        log.info('Desconectado')
+
+    except Exception as exp:
+        log.exception('Falha %s', str(exp))
+
+    log.info('App desconectado')
+
+
+
+if __name__ == '__main__':
+    main()
